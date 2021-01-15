9822390356effae379bff1ebcda276b5d6dee8ce
nazira <nazikkydyralieva@gmail.com>
2021-01-15 12:59:17 +0600

    прописала путь  для заказа

--- sugar/sweet_girl/urls.py
+++ sugar/sweet_girl/urls.py
@@ -7,6 +7,7 @@ router.register('service', ServiceViewSet, basename='service')
 router.register('master', MasterViewSet, basename='master')
 router.register('sertif', CertificatesViewSet, basename='sertif')
 router.register('feed', FeedbackViewSet, basename='feed')
+router.register('spot', SpotViewSet, basename='spot')
 
 
 
